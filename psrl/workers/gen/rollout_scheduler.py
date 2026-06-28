5b2aebd3285444e960ccf7aadfd55a74e59180ff
Somoku <linsh0@protonmail.com>
2026-06-28 14:13:18 +0800

    fix(rollout): fix async scheduling perf bugs by customization on AsyncScheduler
    Signed-off-by: Somoku <linsh0@protonmail.com>

--- psrl/workers/gen/rollout_scheduler.py
+++ psrl/workers/gen/rollout_scheduler.py
@@ -4,7 +4,7 @@ import os
 from vllm.compilation.cuda_graph import CUDAGraphStat
 from vllm.distributed.kv_transfer.kv_connector.v1.metrics import KVConnectorStats
 from vllm.v1.core.sched.output import SchedulerOutput
-from vllm.v1.core.sched.scheduler import Scheduler
+from vllm.v1.core.sched.async_scheduler import AsyncScheduler
 from vllm.v1.engine import EngineCoreEventType
 from vllm.v1.metrics.perf import PerfStats
 from vllm.v1.metrics.stats import PrefixCacheStats, SchedulerStats
@@ -15,7 +15,7 @@ psrl_logger = logging.getLogger(__file__)
 psrl_logger.setLevel(os.getenv("PSRL_LOGGING_LEVEL", "WARN"))
 
 
-class RolloutScheduler(Scheduler):
+class RolloutScheduler(AsyncScheduler):
     def make_stats(
         self,
         spec_decoding_stats: SpecDecodingStats | None = None,
